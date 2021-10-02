dc5b4b52a9921753aae19b9a4cb60ef311d439c2
kisungkim <kksuser@daum.net>
2021-10-03 01:42:38 +0900

    7777주석추가

--- kisung/week7.py
+++ kisung/week7.py
@@ -9,10 +9,21 @@ def add_number(input):
     return sum
 
 ret_val = 0
+# 1) - 를 기준으로 나눠준다. ex 15+30-20 -> 15+30, 20
 parsed  = context.split("-")
+# 2) 첫 - 전 +가 나오는지 확인하고. ex 15+30-20 -> 15 found
+# 해당 값을 두번 더해준다.(logic에서 첫 번째가 +인경우 예외발생하므로)
 if context.find("-") != 0:
     ret_val += add_number(parsed[0])
     ret_val += add_number(parsed[0])
+# 3) 1)에서 나눈 각 식의 숫자합을 빼준다.
+# logic:각 마이너스 앞부터 다음 마이너스까지 괄호를 치는것이 항상 최소값
+# ex 15+30-20 
+# => 2번조건) 15 + 15 
+# => 3번조건) -(15+30) -(20)
+# => 결과) 15 + 15 -(15+30) - (20)
+# =>       15 + (15 - 15) -(30) -(20)
+# 단 이 로직에서 모두 + 만 사용되는 경우는 고려하지 않았다 -> 백준에서는 합격임
 for item in parsed:
     if item == '':
         continue
